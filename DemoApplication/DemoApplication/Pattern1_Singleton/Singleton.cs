c98763c230d60379f151b7ec9961f09c1b0be485
Shradha <sjoshi@ex2india.com>
2019-06-09 00:52:43 +0530

    Singleton pattern

---
+++ DemoApplication/DemoApplication/Pattern1_Singleton/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoApplication.Pattern1_Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance;
+        private static readonly object LockObj = new object();
+
+        private Singleton() {}
+
+        public static Singleton Instanse
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (LockObj)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Singleton();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ DemoApplication/DemoApplication/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DemoApplication.Pattern1_Singleton;
+
+namespace DemoApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Singleton Pattern
+            Singleton s1, s2;
+            s1 = s2 = null;
+            var tasks = new List<Task>();
+
+            Task t1 = Task.Run(() =>
+            {
+                s1 = Singleton.Instanse;
+            });
+            tasks.Add(t1);
+
+            Task t2 = Task.Run(() =>
+            {
+                s2 = Singleton.Instanse;
+            });
+            tasks.Add(t2);
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine(s1 == s2 ? "Only one instance exist" : "Multiple instance exist");
+            #endregion
+        }
+    }
+}
